c18be9734be0f06e4d5dca82edb340543a427e0b
benjamin <bupark82@gmail.com>
2021-07-13 21:29:50 +0900

    7/13 class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import NewModel
+
 
 def hello_world(request):
-    return HttpResponse('Hello World!')
+    if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
+        data_list = NewModel.objects.all()
+        # return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+        # return render(request, 'accountapp/hello_world.html', context={'text': temp})
+        # return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+    else:
+        # return render(request, 'account/hello_world.html', context={'text': 'GET METHOD!'})
+        data_list = NewModel.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
