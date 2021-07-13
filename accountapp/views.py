3e0bac5e5373ba828489723c079037f77857e19d
benjamin <bupark82@gmail.com>
2021-07-13 21:50:19 +0900

    7/13 class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
@@ -16,7 +19,7 @@ def hello_world(request):
         new_model.text = temp
         new_model.save()
 
-        data_list = NewModel.objects.all()
+        # data_list = NewModel.objects.all()
         # return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
         # return render(request, 'accountapp/hello_world.html', context={'text': temp})
         # return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
@@ -26,3 +29,9 @@ def hello_world(request):
         # return render(request, 'account/hello_world.html', context={'text': 'GET METHOD!'})
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
